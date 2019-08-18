8d93534dca61f2dcd54c8b98966f95aeba837553
BaselAkasha <Contact@BaselAkasha.com>
2019-08-18 11:56:59 +0100

    Add input validation

--- script/script.js
+++ script/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
   // Add a gab between the main and the footer
   $("#footerSpace").css("height",$("#footer").height());
+
+
+  $("#convertBtn").click(function(){
+    var text = $("#input").val();
+
+    // Validate Input
+    if(text.length == 0){
+      $("#input").css("border-color","red");
+    }
+    
+  });
+
 });
